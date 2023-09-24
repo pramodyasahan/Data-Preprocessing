f45d030781551e81da617164ba0fb0d8d15796ec
pramodyasahan <sahandil1013@gmail.com>
2023-09-24 15:39:17 +0530

    Taking care of missing data

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :3].values
 y = dataset.iloc[:, -1].values
 
+impute = SimpleImputer(missing_values=np.nan, strategy="mean")
+impute.fit(X[:, 1:3])
+X[:, 1:3] = impute.transform(X[:, 1:3])
+
