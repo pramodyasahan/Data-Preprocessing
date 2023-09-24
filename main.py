bf18185a7819bd0a71d08a521043bf0eacf8135a
pramodyasahan <sahandil1013@gmail.com>
2023-09-24 15:38:21 +0530

    Split data into variables

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.model_selection import train_test_split
 
+dataset = pd.read_csv("Data.csv")
+X = dataset.iloc[:, :3].values
+y = dataset.iloc[:, -1].values
+
