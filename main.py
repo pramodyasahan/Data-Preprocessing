dc61bf0379fd48327fceda92a8a83e79fa35e449
pramodyasahan <sahandil1013@gmail.com>
2023-09-24 15:37:42 +0530

    import libraries

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+
